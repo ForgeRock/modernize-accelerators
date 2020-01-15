55c81f32cfb3fdc8438105927b3dbc792991f5b5
mariantiris <marian.tiris@itsmartsystems.eu>
2020-01-15 15:15:30 +0200

    Adding framework sources

---
+++ forgerock-ig-migration-sso-jit/openig-modernize-library/src/main/java/org/forgerock/openig/modernize/common/User.java
@@ -0,0 +1,80 @@
+/***************************************************************************
+ *  Copyright 2019 ForgeRock AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ ***************************************************************************/
+package org.forgerock.openig.modernize.common;
+
+public class User {
+
+	private String userName;
+	private String userPassword;
+	private String userEmail;
+	private String userFirstName;
+	private String userLastName;
+
+	public User(String userName, String userPassword) {
+		this.setUserName(userName);
+		this.setUserPassword(userPassword);
+	}
+
+	public User() {
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public String getUserEmail() {
+		return userEmail;
+	}
+
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
+	public String getUserFirstName() {
+		return userFirstName;
+	}
+
+	public void setUserFirstName(String userFirstName) {
+		this.userFirstName = userFirstName;
+	}
+
+	public String getUserLastName() {
+		return userLastName;
+	}
+
+	public void setUserLastName(String userLastName) {
+		this.userLastName = userLastName;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userName=" + userName + ", userPassword=" + "******" + ", userEmail=" + userEmail
+				+ ", userFirstName=" + userFirstName + ", userLastName=" + userLastName + "]";
+	}
+
+}
